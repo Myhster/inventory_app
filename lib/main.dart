4be18cb1c47a1262c35555cc5089f25c0dfb4dce
Bob Jones <wertzuio987@web.de>
2025-03-04 19:36:27 +0100

    Change home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:inventory_app/screens/home_screen.dart';
+import 'package:inventory_app/screens/inventory_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +16,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.tealAccent,
         ),
       ),
-      home: const HomeScreen(),
+      home: const InventoryScreen(),
     );
   }
 }
